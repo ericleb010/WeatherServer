1de1e99b01c623975f296ffbf4888ee5d47e35b6
Eric Leblanc <knox3536@gmail.com>
2017-02-28 01:35:36 -0500

    Completed logic for receiving real-time request, sending to API, and returning result

--- server/server.js
+++ server/server.js
@@ -1,26 +1,15 @@
-// System-wide imports
 let server = require("express")();
-let http = require("http");
-let fs = require("fs");
-// Project-wide imports
 let config = require("../config/server_config.js");
 let api = require("./api_services.js");
-
-
-// Get API key for weather GET requests
-const keySuffix = "_api_key";
-const keyTypes = ['OWM'];
-const apiKeys = keyTypes.map(type => 
-    fs.readFileSync("config/" + type + keySuffix, { encoding: "ascii" }));
 
 
 // Define routes for the API
 server.get(api.realTimePath, api.realTimeWeather);
 
 
-console.log("\nNow listening on port " + config.port + "...");
+console.log("\nNow listening on port " + config.PORT + "...");
 console.log("Press CTRL+C to exit.");
-server.listen(config.port);
+server.listen(config.PORT);
 
 // TEST
 // http.get("http://api.openweathermap.org/data/2.5/weather?id=5992996&APPID=" + apiKeys[0], function(res) {
