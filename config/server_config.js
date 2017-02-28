5208e87948eeddc93ee549ccba063ffd5833ea72
Eric Leblanc <ericleb010@gmail.com>
2017-02-28 01:35:36 -0500

    Completed logic for receiving real-time request, sending to API, and returning result

--- config/server_config.js
+++ config/server_config.js
@@ -1,7 +1,31 @@
+let fs = require("fs");
+
+
+const keySuffix = "_api_key";
+const keyTypes = ['OWM'];
+let apiKeys = {};
+keyTypes.forEach(type => 
+    apiKeys[type] = fs.readFileSync("config/" + type + keySuffix, { encoding: "ascii" }));
+
+
+// Return this constant for propagation.
 const config = {
-    port: 4420,
+    PORT: 4420,
     // Paths
-    real_time_path: "/current"
+    REAL_TIME_PATH: "/current/:location",
+    // APIs
+    KEY_TYPES: keyTypes,
+    API_KEYS: apiKeys,
+    API_BASE_URLS: {
+        "OWM": "http://api.openweathermap.org/data/2.5/"
+    },
+    REAL_TIME_QUERY: {
+        "OWM": (loc, key) => "weather?id=" + loc + "&APPID=" + key
+    },
+    // Defaults
+    DEFAULT_LOCATIONS: {
+        "OWM": "5992996"
+    }
 }
 
 module.exports = config;
