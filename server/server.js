4b2341801002e1212719e67a30933a1a7a0b934c
Gogs <gogs@fake.local>
2017-09-04 21:40:05 -0400

    Changed logging to syslog levels and added error handling in real time GET requests

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ let logger = require("winston");
 let config = require("../config/serverConfig.js");
 
 logger.level = config.LOG_LEVEL;
+logger.setLevels(logger.config.syslog.levels);
+
 logger.info("\nNow listening on port " + config.PORT + "...");
 logger.info("Press CTRL+C to exit.");
 server.listen(config.PORT);
