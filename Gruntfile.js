ec24df980477c3ea78558e2cd598f84153fad709
Eric Leblanc <ericleb010@gmail.com>
2017-09-09 14:31:24 -0400

    Forgot commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,10 +7,17 @@ module.exports = function(grunt) {
             test: {
                 src: ["test/**/*.js"]
             }
+        },
+        mocha_istanbul: {
+            coverage: {
+                src: "test"
+            }
         }
     });
 
     grunt.loadNpmTasks("grunt-eslint");
     grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.registerTask('default', ['mochaTest', 'eslint']);
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
+    
+    grunt.registerTask('default', ['mochaTest', 'mocha_istanbul:coverage', 'eslint']);
 }
