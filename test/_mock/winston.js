fb684576fa58a7f3f57ec1211fea3174a82b46b0
Eric Leblanc <ericleb010@gmail.com>
2017-09-09 22:04:23 -0400

    Completed tests for utils file

--- test/_mock/winston.js
+++ test/_mock/winston.js
@@ -1,7 +1,9 @@
-let count;
+let logCounts;
+
+let levels = ["emerg", "alert", "crit", "err", "warning", "notice", "info", "debug"];
 
 function resetCounts() {
-    count = {
+    logCounts = {
         emerg: 0,
         alert: 0,
         crit: 0,
@@ -14,15 +16,26 @@ function resetCounts() {
 }
 
 module.exports = {
-    getLogCounts: (level) => count[level],
-    clearLogCounts: resetCounts,
 
-    emerg: () => count["emerg"]++,
-    alert: () => count["alert"]++,
-    crit: () => count["crit"]++,
-    err: () => count["err"]++,
-    warning: () => count["warning"]++,
-    notice: () => count["notice"]++,
-    info: () => count["info"]++,
-    debug: () => count["debug"]++
+    // If logLevel is a number, return the sum of all logs above that log level.
+    // If it is a string instead, return just that level's count.
+    // If it is not provided, return the whole array (for debugging purposes).
+    getLogCounts: function(logLevel) {
+        if (typeof logLevel === "number") {
+            return levels.reduce(function(prev, curr, i) {
+                return (i <= logLevel) ? prev + logCounts[curr] : prev;
+            }, 0);
+        }
+        return !logLevel ? logCounts : logCounts[logLevel]
+    },
+    clearLogs: resetCounts,
+
+    emerg: () => logCounts["emerg"]++,
+    alert: () => logCounts["alert"]++,
+    crit: () => logCounts["crit"]++,
+    err: () => logCounts["err"]++,
+    warning: () => logCounts["warning"]++,
+    notice: () => logCounts["notice"]++,
+    info: () => logCounts["info"]++,
+    debug: () => logCounts["debug"]++
 }
